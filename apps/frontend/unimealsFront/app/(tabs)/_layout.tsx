e0b066a811cf51cfc81791079014de3432c4ed87
Edgar Martinez <edgarsandoval685@gmail.com>
2025-06-28 16:21:47 -0600

    header

--- apps/frontend/unimealsFront/app/(tabs)/_layout.tsx
+++ apps/frontend/unimealsFront/app/(tabs)/_layout.tsx
@@ -1,46 +1,57 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { useColorScheme } from '@hooks/useColorScheme';
+import { View, StyleSheet } from 'react-native';
 import CustomTabBar from '@components/ui/CustomTabBar';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-
+import Header from '@ui/Header';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      tabBar={(props) => <CustomTabBar {...props} />}
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="feed"
-        options={{
-          title: 'Inicio',
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="home" size={24} color={color} />
-          ),
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Header /> {/* 👈 Este es tu header común para todas las tabs */}
+
+      <Tabs
+        tabBar={(props) => <CustomTabBar {...props} />}
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="plus"
-        options={{
-          title: '',
-          tabBarButton: () => null, // Oculta el tab nativo
-        }}
-      />
-      <Tabs.Screen
-        name="chats"
-        options={{
-          title: 'Chats',
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="message1" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="feed"
+          options={{
+            title: 'Inicio',
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="home" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="plus"
+          options={{
+            title: '',
+            tabBarButton: () => null,
+          }}
+        />
+        <Tabs.Screen
+          name="chats"
+          options={{
+            title: 'Chats',
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="message1" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
